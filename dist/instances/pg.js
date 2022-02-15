faecd593315bacef619f87db892e1d4993916ff6
alissonrangel <alisson.rangel@hotmail.com>
2022-02-15 00:11:52 -0300

    changing sequilize and tsc -w

--- dist/instances/pg.js
+++ dist/instances/pg.js
@@ -7,7 +7,19 @@ exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// });
 exports.sequelize = new sequelize_1.Sequelize(process.env.PG_DB, process.env.PG_USER, process.env.PG_PASSWORD, {
+    host: process.env.PG_HOST,
     dialect: 'postgres',
     port: parseInt(process.env.PG_PORT)
 });
+//export const sequelize = new Sequelize(process.env.DATABASE_URL as string);
