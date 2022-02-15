242bd89f5002cbecaca43c630032b792a25d53c7
alissonrangel <alisson.rangel@hotmail.com>
2022-02-14 22:52:32 -0300

    updating sequilize

--- src/instances/pg.ts
+++ src/instances/pg.ts
@@ -3,12 +3,26 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-    process.env.PG_DB as string,
-    process.env.PG_USER as string,
-    process.env.PG_PASSWORD as string,
-    {
-        dialect: 'postgres',
-        port: parseInt(process.env.PG_PORT as string)
-    }
-);
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// });
+
+// export const sequelize = new Sequelize(
+//     process.env.PG_DB as string,
+//     process.env.PG_USER as string,
+//     process.env.PG_PASSWORD as string,
+//     {
+//         host: process.env.PG_HOST,
+//         dialect: 'postgres',
+//         port: parseInt(process.env.PG_PORT as string)
+//     }
+// );
+
+export const sequelize = new Sequelize(process.env.DATABASE_URL as string);
